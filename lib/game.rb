65ce8792088c93adcfe8343d0fdccc5ea3d1d7b9
CodeByBlazej <codebyblazej@gmail.com>
2024-09-03 18:40:49 +0100

    test new pin object

--- lib/game.rb
+++ lib/game.rb
@@ -5,5 +5,9 @@ require_relative 'sort/player'
 require_relative 'sort/pins'
 
 board = Board.new
+white_pin = Pin.new("white")
+red_pin = Pin.new("red")
 
 board.display_board
+white_pin.show_me
+red_pin.show_me
