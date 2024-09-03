9518c78dfc3a31ac09f9db62ac8a9d40b005e805
CodeByBlazej <codebyblazej@gmail.com>
2024-09-03 19:33:17 +0100

    add symbol for object

--- lib/sort/colors.rb
+++ lib/sort/colors.rb
@@ -1,8 +1,9 @@
 require 'colorize'
 class Colors
-  attr_accessor :color
+  attr_accessor :color, :symbol
 
-  def initialize (color_name)
+  def initialize (color_name, symbol)
     @color = color_name.colorize(color_name.to_sym) 
+    @symbol = symbol.colorize(color_name.to_sym)
   end
 end
