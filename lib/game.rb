8a39c725e1427cecb68d544f9c35b739b257e439
CodeByBlazej <codebyblazej@gmail.com>
2024-09-03 19:32:54 +0100

    create colors object and test it

--- lib/game.rb
+++ lib/game.rb
@@ -8,6 +8,9 @@ board = Board.new
 white_pin = Pin.new("white")
 red_pin = Pin.new("red")
 
+red_ball = Colors.new("red", "X")
+
 board.display_board
 white_pin.show_me
 red_pin.show_me
+puts red_ball.symbol
