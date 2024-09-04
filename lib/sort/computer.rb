d28261f1ea1884b83926697e6037350d71782d7d
CodeByBlazej <codebyblazej@gmail.com>
2024-09-04 19:17:37 +0100

    add method to create another 4 random colors

--- lib/sort/computer.rb
+++ lib/sort/computer.rb
@@ -1,7 +1,15 @@
+require_relative 'colors'
+
 class Computer
-  attr_accessor :name
+  attr_accessor :name, :selected_colors
 
   def initialize (name)
     @name = name
+  end
+
+  
+  def pick_colors(colors)
+    @selected_colors = []
+    @selected_colors.push(colors.sample(4))
   end
 end
