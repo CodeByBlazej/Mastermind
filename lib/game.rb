7dd397b3b10c920c4caec09204f40f279491497d
CodeByBlazej <codebyblazej@gmail.com>
2024-09-04 18:31:01 +0100

    create 8 balls and test yellow color

--- lib/game.rb
+++ lib/game.rb
@@ -9,8 +9,16 @@ white_pin = Pin.new("white")
 red_pin = Pin.new("red")
 
 red_ball = Colors.new("red", "X")
+green_ball = Colors.new("green", "X")
+yellow_ball = Colors.new("yellow", "X")
+blue_ball = Colors.new("blue", "X")
+purple_ball = Colors.new("magenta", "X")
+cyan_ball = Colors.new("cyan", "X")
+white_ball = Colors.new("white", "X")
+grey_ball = Colors.new("grey", "X")
 
 board.display_board
 white_pin.show_me
 red_pin.show_me
 puts red_ball.symbol
+puts yellow_ball.symbol
