ee675097bd9a7c85d384976f4d118d1e44d3867e
CodeByBlazej <codebyblazej@gmail.com>
2024-09-07 13:09:47 +0100

    add selected colors with names to display

--- lib/sort/board.rb
+++ lib/sort/board.rb
@@ -11,14 +11,20 @@ class Board
   end
 
 
-  def display_board
+  def display_board(computer)
     puts "-------"
+    puts "COLORS YOU ARE PLAYING WIH:"
+    computer.show_selected_colors(@selected_colors)
     arr1 = @hidden_row.map { |color| color.nil? ? "[0]" : "[#{color.symbol}]" }
+    
     arr2 = @guess_row.map { |color| color.nil? ? "[0]" : "[#{color.symbol}]" }
     arr3 = @hint_row[0].map { |color| color.nil? ? "[0]" : "[#{color.color_symbol}]"}
     arr4 = @hint_row[1].map { |color| color.nil? ? "[0]" : "[#{color.color_symbol}]"}
-
+    
+    puts "HIDDEN ROW FOR NOW"
     puts arr1.join(' ')
+    puts "HIDDEN ROW FOR NOW"
+
     puts arr2.join(' ')
     puts "Pins are:"
     puts arr3.join(' ')
