2c644ba5aa6e87c60034235a78c87491619ed236
CodeByBlazej <codebyblazej@gmail.com>
2024-09-07 09:05:59 +0100

    adjust method to be more readible

--- lib/sort/colors.rb
+++ lib/sort/colors.rb
@@ -3,10 +3,11 @@ require_relative 'computer'
 require_relative 'board'
 
 class Colors
-  attr_accessor :color, :symbol
+  attr_accessor :color_name, :symbol
 
   def initialize (color_name, symbol)
-    @color = color_name.colorize(color_name.to_sym) 
+    @color_name = color_name.colorize(color_name.to_sym) 
     @symbol = symbol.colorize(color_name.to_sym)
   end
 end
+
