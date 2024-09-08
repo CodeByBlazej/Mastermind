ed9699c13bd45b71fe3a253b8f6ff80f2fac76b5
CodeByBlazej <codebyblazej@gmail.com>
2024-09-08 19:41:14 +0100

    place sel_colors to initialize solving errors

--- lib/sort/computer.rb
+++ lib/sort/computer.rb
@@ -1,12 +1,14 @@
 require_relative 'colors'
 require_relative 'board'
+require_relative 'player'
 
 
 class Computer
-  attr_accessor :name, :selected_colors, :code
+  attr_accessor :name, :selected_colors, :code, :guessed_code
 
   def initialize (name)
     @name = name
+    @selected_colors = []
   end
 
   
@@ -14,12 +16,22 @@ class Computer
     @selected_colors = colors.sample(4)
   end
 
-  def make_a_code(selected_colors)
+  def make_a_code
     @code = @selected_colors.sample(4)
   end
 
-  def show_selected_colors(selected_colors)
-    @selected_colors.map { |color| puts "#{color.symbol} - #{color.color_name}"}
+  def show_selected_colors
+    @selected_colors.each do |color|
+      colorized_name = color.color_name.colorize(color.color_name.to_sym)
+      puts "#{color.symbol} - #{colorized_name}"
+    end
+  end
+
+  def guess
+    @guessed_code = Array.new(4) do
+      guess_input = gets.chomp.downcase
+      @selected_colors.find { |color| color.color_name == guess_input}
+    end
   end
   
 end
