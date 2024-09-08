152a3be21da3c26d958b3750959c1360cafc4262
CodeByBlazej <codebyblazej@gmail.com>
2024-09-08 20:21:25 +0100

    remodel

--- lib/sort/player.rb
+++ lib/sort/player.rb
@@ -1,8 +1,14 @@
+require_relative 'computer'
+
 class Player
-  attr_accessor :name
+  attr_accessor :name, :guessed_code
 
   def initialize (name)
     @name = name
+  end
+
+  def guess(selected_colors)
+    @guesed_code = @selected_colors.map { |color| color.color_name == gets.chomp ? color.symbol : "You put wrong name" }
   end
 
 end
