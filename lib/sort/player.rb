7b02759a465360a8bf0d131f3e8f52b5b00a00d6
CodeByBlazej <codebyblazej@gmail.com>
2024-09-14 19:57:15 +0100

    moves play round method to player class

--- lib/sort/player.rb
+++ lib/sort/player.rb
@@ -1,4 +1,5 @@
 require_relative 'computer'
+require_relative 'board'
 
 class Player
   attr_accessor :name, :guessed_code
@@ -14,4 +15,21 @@ class Player
     end
   end
 
+  def play_round(board, all_colors, white_pin, red_pin)
+    round = (1..12)
+    round.each do |round|
+
+      self.guess(all_colors)
+      board.guess_row = @guessed_code
+      board.check_for_hints(white_pin, red_pin)
+      board.display_board(all_colors)
+      puts "Guess the code by putting each peg in every hole. 
+      To do it type name of the color: "
+      # binding.pry
+      if board.hint_row.all?(red_pin)
+        puts "#{@name} WON!"
+        break
+      end
+    end
+  end
 end
