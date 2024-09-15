16e272a428fa6cc007693259614b9e9e0071e44d
CodeByBlazej <codebyblazej@gmail.com>
2024-09-15 09:17:06 +0100

    move play round method back to game file

--- lib/sort/player.rb
+++ lib/sort/player.rb
@@ -15,21 +15,4 @@ class Player
     end
   end
 
-  def play_round(board, all_colors, white_pin, red_pin)
-    round = (1..12)
-    round.each do |round|
-
-      self.guess(all_colors)
-      board.guess_row = @guessed_code
-      board.check_for_hints(white_pin, red_pin)
-      board.display_board(all_colors)
-      puts "Guess the code by putting each peg in every hole. 
-      To do it type name of the color: "
-      # binding.pry
-      if board.hint_row.all?(red_pin)
-        puts "#{@name} WON!"
-        break
-      end
-    end
-  end
 end
