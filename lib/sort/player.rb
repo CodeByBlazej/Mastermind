7c1953e2a7b42b0183e51cbe9871e1fdfd70b65b
CodeByBlazej <codebyblazej@gmail.com>
2024-09-15 13:22:17 +0100

    add method to make a code

--- lib/sort/player.rb
+++ lib/sort/player.rb
@@ -2,7 +2,7 @@ require_relative 'computer'
 require_relative 'board'
 
 class Player
-  attr_accessor :name, :guessed_code
+  attr_accessor :name, :guessed_code, :code
 
   def initialize (name)
     @name = name
@@ -11,7 +11,20 @@ class Player
   def guess(all_colors)
     @guessed_code = Array.new(4) do
       guess_input = gets.chomp.downcase
-      all_colors.find { |color| color.color_name == guess_input}
+      all_colors.find { |color| color.color_name == guess_input }
+    end
+  end
+
+  def make_a_code(all_colors)
+    @code = Array.new(4) do
+      code_input = gets.chomp.downcase
+      all_colors.find { |color| color.color_name == code_input }
+      
+    end
+
+    if @code.include?(nil)
+      puts "Try again, make sure all color names are written correctly"
+      make_a_code(all_colors)
     end
   end
 
