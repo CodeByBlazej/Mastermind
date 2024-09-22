153404239678886f5f29552e6c34848772c166ea
CodeByBlazej <codebyblazej@gmail.com>
2024-09-22 11:51:57 +0100

    clean the code and add description

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,7 @@
+# Game class is responsible for all game mechanics and sarting the game
+
 require 'pry-byebug'
+require 'colorize'
 require_relative 'sort/board'
 require_relative 'sort/colors'
 require_relative 'sort/computer'
@@ -26,13 +29,14 @@ class Game
     @player = Player.new(@player_name)
     @computer = Computer.new("computer")
     @round_number = 1
+    @maker = true
   end
 
   def play_guesser_round
     while @round_number <= 12 do
       @player.guess(@all_colors)
       @board.guess_row = @player.guessed_code
-      @board.player_check_for_hints(@white_pin, @red_pin)
+      @board.player_check_hints(@white_pin, @red_pin)
       
       @board.display_board(@all_colors)
       
@@ -70,35 +74,35 @@ class Game
       
       @board.guess_row = @computer.guessed_code
       puts "\nIt's round number: #{@round_number}"
-      @board.display_board(@all_colors)
+      @board.display_board(@all_colors, @maker)
       @board.player_put_the_pins(@white_pin, @red_pin)
       @board.hint_row = @board.player_selected_pins
-      @board.computer_check_for_hints(@white_pin, @red_pin)
-      @board.display_board(@all_colors)
+      @board.computer_check_hints(@white_pin, @red_pin)
+      @board.display_board(@all_colors, @maker)
       
       
       if @board.hint_row.all?(@red_pin)
         puts "\nGame over! #{@computer.name} wins in round #{@round_number}!"
         break
       end
+
       @round_number += 1
       
       if @round_number > 12
         puts "\nGame over! The maximum number of rounds has been reached!"
         break
       end
-      # puts "\nIt's round number: #{@round_number}"
     end
   end
     
   def start
     puts "Welcome to Mastermind! What is your name?"
     @player_name = gets.chomp
-    puts "Thanks! Do you want to be code maker or code guesser? \nType MAKER or GUESSER"
+    puts "\nThanks! Do you want to be code maker or code guesser? \nType MAKER or GUESSER"
     @selection = gets.chomp.downcase
     
     if @selection == "guesser"
-      puts "Thanks! Computer is going to make a code now..."
+      puts "\nThanks! Computer is going to make a code now..."
       @computer.make_a_code(@all_colors)
       @board.hidden_row = @computer.code 
       puts "\nIt's round number: #{@round_number}"
@@ -106,15 +110,14 @@ class Game
       puts "\nGuess the code by putting each peg in every hole. \nTo do it type name of the color: "
       play_guesser_round
     end
-
+      
     if @selection == "maker"
-      puts "Thanks! Make a code from available colors..."
+      puts "\nThanks! Make a code from available colors..."
       @board.show_available_colors(@all_colors)
       @player.make_a_code(@all_colors)
       @board.hidden_row = @player.code
-      # puts "\nIt's round number: #{@round_number}"
-      @board.display_board(@all_colors)
-      puts "Computer is guessing now..."
+      @board.display_board(@all_colors, @maker)
+      puts "\nComputer is guessing now..."
       computer_guesser_round
     end
   end
