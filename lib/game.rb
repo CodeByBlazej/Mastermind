36267db00b3a5eb96b80f8f5479844cd7b26aa8e
CodeByBlazej <codebyblazej@gmail.com>
2024-09-22 12:41:26 +0100

    edit file regarding to RUBOCOP guidlines

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,18 @@
+require "pry-byebug"
+require "colorize"
+require_relative "sort/board"
+require_relative "sort/colors"
+require_relative "sort/computer"
+require_relative "sort/player"
+require_relative "sort/pins"
+
 # Game class is responsible for all game mechanics and sarting the game
-
-require 'pry-byebug'
-require 'colorize'
-require_relative 'sort/board'
-require_relative 'sort/colors'
-require_relative 'sort/computer'
-require_relative 'sort/player'
-require_relative 'sort/pins'
-
 class Game
-
   def initialize
     @board = Board.new
     @white_pin = Pin.new("white", "W")
     @red_pin = Pin.new("red", "R")
-    
+
     @all_colors = [
       Colors.new("red", "X"),
       Colors.new("green", "X"),
@@ -33,20 +31,20 @@ class Game
   end
 
   def play_guesser_round
-    while @round_number <= 12 do
+    while @round_number <= 12
       @player.guess(@all_colors)
       @board.guess_row = @player.guessed_code
       @board.player_check_hints(@white_pin, @red_pin)
-      
+
       @board.display_board(@all_colors)
-      
+
       if @board.hint_row.all?(@red_pin)
         puts "\nGame over! #{@player_name} wins in round #{@round_number}!"
         break
       end
-      
+
       @round_number += 1
-      
+
       if @round_number > 12
         puts "\nGame over! The maximum number of rounds has been reached!"
         break
@@ -59,19 +57,16 @@ class Game
   end
 
   def computer_guesser_round
-    while @round_number <= 12 do
-      
-      if @board.hint_row.any?
-        @board.hint_row = @board.hint_row.map { |slot| slot = nil }
-      end
-      
+    while @round_number <= 12
+
+      @board.hint_row = @board.hint_row.map { |slot| slot = nil } if @board.hint_row.any?
+
       if @board.possible_current_colors.nil?
         @computer.guess(@all_colors)
       else
         @computer.guess_smart(@all_colors, @board.possible_current_colors)
       end
-      
-      
+
       @board.guess_row = @computer.guessed_code
       puts "\nIt's round number: #{@round_number}"
       @board.display_board(@all_colors, @maker)
@@ -79,47 +74,46 @@ class Game
       @board.hint_row = @board.player_selected_pins
       @board.computer_check_hints(@white_pin, @red_pin)
       @board.display_board(@all_colors, @maker)
-      
-      
+
       if @board.hint_row.all?(@red_pin)
         puts "\nGame over! #{@computer.name} wins in round #{@round_number}!"
         break
       end
 
       @round_number += 1
-      
+
       if @round_number > 12
         puts "\nGame over! The maximum number of rounds has been reached!"
         break
       end
     end
   end
-    
+
   def start
     puts "Welcome to Mastermind! What is your name?"
     @player_name = gets.chomp
     puts "\nThanks! Do you want to be code maker or code guesser? \nType MAKER or GUESSER"
     @selection = gets.chomp.downcase
-    
+
     if @selection == "guesser"
       puts "\nThanks! Computer is going to make a code now..."
       @computer.make_a_code(@all_colors)
-      @board.hidden_row = @computer.code 
+      @board.hidden_row = @computer.code
       puts "\nIt's round number: #{@round_number}"
       @board.display_board(@all_colors)
       puts "\nGuess the code by putting each peg in every hole. \nTo do it type name of the color: "
       play_guesser_round
     end
-      
-    if @selection == "maker"
-      puts "\nThanks! Make a code from available colors..."
-      @board.show_available_colors(@all_colors)
-      @player.make_a_code(@all_colors)
-      @board.hidden_row = @player.code
-      @board.display_board(@all_colors, @maker)
-      puts "\nComputer is guessing now..."
-      computer_guesser_round
-    end
+
+    return unless @selection == "maker"
+
+    puts "\nThanks! Make a code from available colors..."
+    @board.show_available_colors(@all_colors)
+    @player.make_a_code(@all_colors)
+    @board.hidden_row = @player.code
+    @board.display_board(@all_colors, @maker)
+    puts "\nComputer is guessing now..."
+    computer_guesser_round
   end
 end
 
